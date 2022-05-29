76013f2562141652d2ca64312d38023bcadecf8d
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-05-29 17:44:18 +0300

    429 page updated

--- src/pages/errorPages/tooManyRequests/tooManyRequests.tsx
+++ src/pages/errorPages/tooManyRequests/tooManyRequests.tsx
@@ -4,12 +4,20 @@ export const TooManyRequests = ()=>{
     return(<>
         <div className={styles.wrapper}>
             <div className={styles.content}>
+                <div className={styles.icon}>
+                    <Icon height={150} name={"sadFace"} width={150}/>
+                </div>
                 <h1 className={styles.title}>
                     429
                 </h1>
-                <div className={styles.icon}>
-                    <Icon height={230} name={"sadFace"} width={230}/>
-                </div>
+                <h2 className={styles.description}>
+                    <p>
+                        Too many requests.
+                    </p>
+                    <p>
+                        Try to reduce the number of requests per second.
+                    </p>
+                </h2>
             </div>
         </div>
     </>)
